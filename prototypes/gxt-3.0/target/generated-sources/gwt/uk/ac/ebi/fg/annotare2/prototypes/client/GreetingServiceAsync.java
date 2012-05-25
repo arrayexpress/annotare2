6b6b1fc62c53d7463a5663e9b042c6a7a235c741
Nikolay Kolesnikov <kolais@ebi.ac.uk>
2012-05-25 13:52:52 +0100

    Added minimal "Hello, world!" sample in GWT

---
+++ prototypes/gxt-3.0/target/generated-sources/gwt/uk/ac/ebi/fg/annotare2/prototypes/client/GreetingServiceAsync.java
@@ -0,0 +1,40 @@
+package uk.ac.ebi.fg.annotare2.prototypes.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+
+public interface GreetingServiceAsync
+{
+
+    /**
+     * GWT-RPC service  asynchronous (client-side) interface
+     * @see uk.ac.ebi.fg.annotare2.prototypes.client.GreetingService
+     */
+    void greetServer( java.lang.String name, AsyncCallback<java.lang.String> callback );
+
+
+    /**
+     * Utility class to get the RPC Async interface from client-side code
+     */
+    public static final class Util 
+    { 
+        private static GreetingServiceAsync instance;
+
+        public static final GreetingServiceAsync getInstance()
+        {
+            if ( instance == null )
+            {
+                instance = (GreetingServiceAsync) GWT.create( GreetingService.class );
+                ServiceDefTarget target = (ServiceDefTarget) instance;
+                target.setServiceEntryPoint( GWT.getModuleBaseURL() + "greet" );
+            }
+            return instance;
+        }
+
+        private Util()
+        {
+            // Utility class should not be instanciated
+        }
+    }
+}
