19d686bf1ac5c0bb4b4017051a623597102bba1f
Olga Melnichuk <olkin@ebi.ac.uk>
2012-05-29 17:58:30 +0100

    playing with layout - list of experiments

--- prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/layoutapp/client/LayoutApp.java
+++ prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/layoutapp/client/LayoutApp.java
@@ -2,10 +2,14 @@ package uk.ac.ebi.fg.annotare2.prototypes.layoutapp.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.HasWidgets;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.MouseEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
 import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
 
 /**
@@ -13,22 +17,44 @@ import com.sencha.gxt.widget.core.client.box.AlertMessageBox;
  */
 public class LayoutApp implements EntryPoint {
 
+    interface Binder extends UiBinder<DockPanel, LayoutApp> { }
+    private static final Binder binder = GWT.create(Binder.class);
+
+    @UiField ListBox experimentList;
+
+    @UiField Button newExperimentBtn;
+
     public void onModuleLoad() {
         setUncaughtExceptionHandler();
         loadModule(RootPanel.get());
     }
 
     public void loadModule(final HasWidgets hasWidgets) {
-        ListBox projectList = new ListBox(false);
-        projectList.addItem("All projects");
-        projectList.addItem("Project 2");
-        projectList.addItem("Project 3");
-        projectList.addItem("Project 4");
+   /*     ListBox experimentList = new ListBox(false);
+        experimentList.addItem("All projects");
+        experimentList.addItem("Project 2");
+        experimentList.addItem("Project 3");
+        experimentList.addItem("Project 4");
 
         HorizontalPanel hPanel = new HorizontalPanel();
-        hPanel.add(projectList);
+        hPanel.add(experimentList);*/
 
+        DockPanel hPanel = binder.createAndBindUi(this);
+        experimentList.addItem("All my experiments");
+        experimentList.addItem("Experiment 2");
+        experimentList.addItem("Experiment 3");
+        experimentList.addItem("Experiment 4");
         hasWidgets.add(hPanel);
+    }
+
+    @UiHandler("experimentList")
+    protected void projectSelect(ChangeEvent event) {
+        Window.alert("you've selected " + experimentList.getValue(experimentList.getSelectedIndex()));
+    }
+
+    @UiHandler("newExperimentBtn")
+    protected void newExperimentClick(ClickEvent event) {
+        Window.alert("Create new experiment");
     }
 
     private void setUncaughtExceptionHandler() {
