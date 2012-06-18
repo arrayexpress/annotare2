0cb98224374ba367d9c2fff37fc20a7766144959
Olga Melnichuk <olga.melnichuk@gmail.com>
2012-06-18 13:06:33 +0100

    simple layout added..

--- app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/client/user/UserApp.java
+++ app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/client/user/UserApp.java
@@ -16,20 +16,56 @@
 
 package uk.ac.ebi.fg.annotare2.web.client.user;
 
+import com.google.gwt.activity.shared.ActivityManager;
+import com.google.gwt.activity.shared.ActivityMapper;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.gwt.place.shared.PlaceHistoryHandler;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.web.bindery.event.shared.EventBus;
+import uk.ac.ebi.fg.annotare2.web.client.user.gin.UserAppGinjector;
+import uk.ac.ebi.fg.annotare2.web.client.user.mvp.UserAppPlaceHistoryMapper;
+import uk.ac.ebi.fg.annotare2.web.client.user.place.SubmissionListPlace;
+import uk.ac.ebi.fg.annotare2.web.client.user.mvp.UserAppPlaceFactory;
+import uk.ac.ebi.fg.annotare2.web.client.user.view.widget.AppLayout;
 
 /**
  * @author Olga Melnichuk
  */
 public class UserApp implements EntryPoint {
 
+    private final UserAppGinjector injector = GWT.create(UserAppGinjector.class);
+
+    private AppLayout appWidget = new AppLayout();
+
     public void onModuleLoad() {
         loadModule(RootPanel.get());
     }
 
     private void loadModule(RootPanel root) {
-        Window.alert("Hi!");
+        EventBus eventBus = injector.getEventBus();
+        PlaceController placeController = injector.getPlaceController();
+
+        ActivityMapper leftMenuActivityMapper = injector.getLeftMenuActivityMapper();
+        ActivityManager leftMenuActivityManager = new ActivityManager(leftMenuActivityMapper, eventBus);
+        leftMenuActivityManager.setDisplay(appWidget.getWestPanel());
+
+        ActivityMapper contentActivityMapper = injector.getContentActivityMapper();
+        ActivityManager contentActivityManager = new ActivityManager(contentActivityMapper, eventBus);
+        contentActivityManager.setDisplay(appWidget.getCenterPanel());
+
+        UserAppPlaceFactory factory = injector.getPlaceFactory();
+        SubmissionListPlace defaultPlace = factory.getSubmissionListPlace();
+
+        UserAppPlaceHistoryMapper historyMapper = GWT.create(UserAppPlaceHistoryMapper.class);
+        historyMapper.setFactory(factory);
+
+        PlaceHistoryHandler historyHandler = new PlaceHistoryHandler(historyMapper);
+        historyHandler.register(placeController, eventBus, defaultPlace);
+
+        root.add(appWidget);
+
+        historyHandler.handleCurrentHistory();
     }
 }
