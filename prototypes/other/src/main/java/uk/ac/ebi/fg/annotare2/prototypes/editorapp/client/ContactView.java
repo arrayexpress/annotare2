de3e7ea08ba6c74021cc77e81200e84b2ed690ec
Olga Melnichuk <olga.melnichuk@gmail.com>
2012-07-09 09:44:02 +0100

    prototyping IDF contact view

--- prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/editorapp/client/ContactView.java
+++ prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/editorapp/client/ContactView.java
@@ -4,9 +4,10 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiConstructor;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.*;
-
-import java.util.Date;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DisclosurePanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author Olga Melnichuk
@@ -20,11 +21,68 @@ public class ContactView extends Composite implements IsWidget {
     @UiField
     DisclosurePanel disclosurePanel;
 
+    @UiField
+    TextBoxWithPlaceHolder firstNameBox;
+
+    @UiField
+    TextBoxWithPlaceHolder initialsBox;
+
+    @UiField
+    TextBoxWithPlaceHolder lastNameBox;
+
+    private Header header = new Header();
+
     @UiConstructor
     public ContactView() {
         initWidget(Binder.BINDER.createAndBindUi(this));
+    }
 
-        //contactName.setText("Contact Name " + new Date().getTime());
-        disclosurePanel.getHeaderTextAccessor().setText("Contact Name " + new Date().getTime());
+    private void updateHeader() {
+        disclosurePanel.getHeaderTextAccessor().setText(header.toString());
+    }
+
+    public void setFirstName(String name) {
+        firstNameBox.setValue(name);
+        header.setFirstName(name);
+        updateHeader();
+    }
+
+    public void setLastName(String name) {
+        lastNameBox.setValue(name);
+        header.setLastName(name);
+        updateHeader();
+    }
+
+    public void setInitials(String initials){
+        initialsBox.setValue(initials);
+        header.setInitials(initials);
+        updateHeader();
+    }
+
+    private static class Header {
+        private String firstName = "";
+        private String lastName = "";
+        private String initials = "";
+
+        @Override
+        public String toString() {
+            return isEmpty() ? "Empty" : firstName + " " + initials + " " + lastName;
+        }
+
+        private boolean isEmpty() {
+            return firstName.isEmpty() && lastName.isEmpty() && initials.isEmpty();
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName == null ? "" : firstName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName == null ? "" : lastName;
+        }
+
+        public void setInitials(String initials) {
+            this.initials = initials == null ? "" : initials;
+        }
     }
 }
--- prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/editorapp/client/TextBoxWithPlaceHolder.java
+++ prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/editorapp/client/TextBoxWithPlaceHolder.java
@@ -1,12 +1,14 @@
 package uk.ac.ebi.fg.annotare2.prototypes.editorapp.client;
 
 import com.google.gwt.event.dom.client.*;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.*;
 
 /**
  * @author Olga Melnichuk
  */
-public class TextBoxWithPlaceHolder extends Composite implements IsWidget {
+public class TextBoxWithPlaceHolder extends Composite implements IsWidget, HasValue<String> {
 
     private FocusPanel panel;
 
@@ -20,6 +22,7 @@ public class TextBoxWithPlaceHolder extends Composite implements IsWidget {
         textBox = new TextBox();
         placeHolder = new Label(defaultText);
         panel = new FocusPanel();
+        panel.setStyleName("my-TextBoxWithPlaceHolder");
         panel.add(placeHolder);
 
         panel.addFocusHandler(new FocusHandler() {
@@ -31,16 +34,17 @@ public class TextBoxWithPlaceHolder extends Composite implements IsWidget {
                 textBox.setValue(v);
                 panel.remove(placeHolder);
                 panel.add(textBox);
+                textBox.setFocus(true);
             }
         });
 
-        panel.addBlurHandler(new BlurHandler() {
+        textBox.addBlurHandler(new BlurHandler() {
             public void onBlur(BlurEvent event) {
                 String v = textBox.getValue();
                 if (v.isEmpty()) {
                     v = defaultText;
                 }
-                placeHolder.setText(v);
+                setPlaceHolderValue(v);
                 panel.remove(textBox);
                 panel.add(placeHolder);
             }
@@ -50,7 +54,45 @@ public class TextBoxWithPlaceHolder extends Composite implements IsWidget {
     }
 
     public void setPlaceHolder(String defaultText) {
+        String v = placeHolder.getText();
+        String prevDefaultText = this.defaultText;
         this.defaultText = defaultText;
+        if (v.equals(prevDefaultText)) {
+            setPlaceHolderValue(defaultText);
+        }
     }
 
+    public String getValue() {
+        return textBox.getValue();
+    }
+
+    public void setValue(String value) {
+        textBox.setValue(value);
+        setPlaceHolderValue(value);
+    }
+
+    public void setValue(String value, boolean fireEvents) {
+        textBox.setValue(value, fireEvents);
+        setPlaceHolderValue(value);
+    }
+
+    private void setPlaceHolderValue(String value) {
+        placeHolder.setText(value);
+        if (defaultText.equals(value)) {
+            placeHolder.addStyleName("empty");
+        } else {
+            placeHolder.removeStyleName("empty");
+        }
+    }
+
+    public HandlerRegistration addValueChangeHandler(ValueChangeHandler<String> stringValueChangeHandler) {
+        return textBox.addValueChangeHandler(stringValueChangeHandler);
+    }
+
+    @Override
+    public void setWidth(String width) {
+        super.setWidth(width);
+        textBox.setWidth(width);
+        placeHolder.setWidth(width);
+    }
 }
