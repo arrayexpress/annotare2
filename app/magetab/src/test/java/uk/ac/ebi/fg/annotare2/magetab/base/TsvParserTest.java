7ff6745f926b6df28454558273c0a12583775766
Olga Melnichuk <olkin@ebi.ac.uk>
2012-08-06 16:31:19 +0100

    added TsvParserTest

---
+++ app/magetab/src/test/java/uk/ac/ebi/fg/annotare2/magetab/base/TsvParserTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2009-2012 European Molecular Biology Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impl
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.fg.annotare2.magetab.base;
+
+import com.google.common.base.Charsets;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.google.common.base.Joiner.on;
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Olga Melnichuk
+ */
+public class TsvParserTest {
+
+    @Test
+    public void test() throws IOException {
+        List<List<String>> rows = new ArrayList<List<String>>() {{
+            add(asList("1", "2"));
+            add(asList("3", "", "4", ""));
+            add(Collections.<String>emptyList());
+            add(asList(""));
+        }};
+
+        StringBuilder sb = new StringBuilder();
+        for (List<String> row : rows) {
+            sb.append(on("\t").join(row)).append("\n");
+        }
+
+        Table table = (new TsvParser()).parse(new ByteArrayInputStream(sb.toString().getBytes(Charsets.UTF_8)));
+
+        assertEquals(rows.size(), table.getRowCount());
+        for (int i = 0; i < table.getRowCount(); i++) {
+            List<String> row = rows.get(i);
+            if (row.isEmpty()) {
+                assertEquals(0, table.lastColumnIndex(i));
+            }
+            for (int j = 0; j < row.size(); j++) {
+                String value = row.get(j);
+                if (isNullOrEmpty(value)) {
+                    assertNull(table.getValueAt(i, j));
+                } else {
+                    assertEquals(value, table.getValueAt(i, j).getValue());
+                }
+            }
+        }
+    }
+}
