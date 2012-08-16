30d149767c725f8645e37f295a11b77f4dfa6dea
Olga Melnichuk <olkin@ebi.ac.uk>
2012-08-16 09:51:24 +0100

    parse(..) method moved out from Investigation class as it is not compatible with GWT

---
+++ app/magetab/src/main/java/uk/ac/ebi/fg/annotare2/magetab/idf/IdfParser.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2009-2012 European Molecular Biology Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impl
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.fg.annotare2.magetab.idf;
+
+import uk.ac.ebi.fg.annotare2.magetab.base.TsvParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Olga Melnichuk
+ */
+public class IdfParser {
+
+    public static Investigation parse(InputStream in) throws IOException {
+        return new Investigation(new TsvParser().parse(in));
+    }
+
+}
--- app/magetab/src/main/java/uk/ac/ebi/fg/annotare2/magetab/idf/Investigation.java
+++ app/magetab/src/main/java/uk/ac/ebi/fg/annotare2/magetab/idf/Investigation.java
@@ -17,14 +17,14 @@
 package uk.ac.ebi.fg.annotare2.magetab.idf;
 
 import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Collections2;
-import uk.ac.ebi.fg.annotare2.magetab.base.*;
+import uk.ac.ebi.fg.annotare2.magetab.base.Row;
+import uk.ac.ebi.fg.annotare2.magetab.base.RowTag;
+import uk.ac.ebi.fg.annotare2.magetab.base.Table;
+import uk.ac.ebi.fg.annotare2.magetab.base.TableCell;
 
 import javax.annotation.Nullable;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -74,7 +74,6 @@ public class Investigation {
 
     private TermSourceList termSourceList;
 
-
     private Table table;
 
     public Investigation() {
@@ -91,12 +90,8 @@ public class Investigation {
         if (generalInfoList.isEmpty()) {
             generalInfoList.add();
         }
-    }
 
-    public static Investigation parse(InputStream in) throws IOException {
-        Investigation table = new Investigation(new TsvParser().parse(in));
-        table.check();
-        return table;
+        //check();
     }
 
     public void check() {
