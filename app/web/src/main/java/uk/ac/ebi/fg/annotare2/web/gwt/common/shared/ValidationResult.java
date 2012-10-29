a97c2ff00cc31759ca894140929bb0756d0a4e60
olgamelnichuk <olkin@ebi.ac.uk>
2012-10-29 17:44:20 +0000

    added ValidationResult object

---
+++ app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/common/shared/ValidationResult.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009-2012 European Molecular Biology Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impl
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.ebi.fg.annotare2.web.gwt.common.shared;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * @author Olga Melnichuk
+ */
+public class ValidationResult implements Serializable {
+
+    private ArrayList<String> errors = new ArrayList<String>();
+
+    private ArrayList<String> warnings = new ArrayList<String>();
+
+    public ValidationResult() {
+    }
+
+    public ValidationResult(ArrayList<String> errors, ArrayList<String> warnings) {
+        this.errors.addAll(errors);
+        this.warnings.addAll(warnings);
+    }
+
+    public boolean hasErrors() {
+        return errors.isEmpty();
+    }
+
+    public ArrayList<String> getErrors() {
+        return errors;
+    }
+
+    public ArrayList<String> getWarnings() {
+        return warnings;
+    }
+}
