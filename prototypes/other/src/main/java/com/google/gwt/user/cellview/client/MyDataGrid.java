c848480a5a88fd73b2403901b91fdc46359b80db
olgamelnichuk <olkin@ebi.ac.uk>
2013-03-11 17:09:06 +0000

    prototyping sdrf section view: added custom datagrid

---
+++ prototypes/other/src/main/java/com/google/gwt/user/cellview/client/MyDataGrid.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2009-2013 European Molecular Biology Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impl
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.gwt.user.cellview.client;
+
+
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.client.TableSectionElement;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.CustomScrollPanel;
+import com.google.gwt.user.client.ui.HeaderPanel;
+
+import java.util.List;
+
+/**
+ * DataGrid with resizable columns.
+ *
+ * @author Olga Melnichuk
+ */
+public class MyDataGrid<T> extends DataGrid<T> {
+
+    private int minWidth;
+
+    private boolean hasLastColumn = false;
+
+    public MyDataGrid(int pageSize, Resources resources) {
+        super(pageSize, resources);
+        getTableHeadElement().getParentElement().getStyle().setProperty("borderCollapse", "collapse");
+        getTableBodyElement().getParentElement().getStyle().setProperty("borderCollapse", "collapse");
+        getTableFootElement().getParentElement().getStyle().setProperty("borderCollapse", "collapse");
+    }
+
+    @Override
+    public void setRowData(int start, List<? extends T> values) {
+        HeaderPanel header = (HeaderPanel) getWidget();
+        final CustomScrollPanel scrollPanel = (CustomScrollPanel) header.getContentWidget();
+        final int hPos = scrollPanel.getHorizontalScrollbar().getHorizontalScrollPosition();
+
+        super.setRowData(start, values);
+
+        Scheduler.get().scheduleDeferred(new Command() {
+            public void execute() {
+                scrollPanel.setHorizontalScrollPosition(hPos);
+            }
+        });
+    }
+
+    public int getColumnCount2() {
+        return getColumnCount() == 0 ? 0 : getColumnCount() - 1;
+    }
+
+    public void setMinimumTableWidthInPx(int width) {
+        super.setMinimumTableWidth(width, com.google.gwt.dom.client.Style.Unit.PX);
+        minWidth = width;
+    }
+
+    public void addColumn(String title, Column<T, ?> column) {
+        if (!hasLastColumn) {
+            Column<T, ?> lastColumn = new Column<T, String>(new TextCell()) {
+                @Override
+                public String getValue(T object) {
+                    return "";
+                }
+            };
+            addColumn(lastColumn, new MyResizableHeader<T>("", lastColumn, this));
+            hasLastColumn = true;
+        }
+        addColumn(column, new MyResizableHeader<T>(title, column, this));
+    }
+
+    @Override
+    public void insertColumn(int beforeIndex, Column<T, ?> col, Header<?> header, Header<?> footer) {
+        if (getColumnCount() > 0 && beforeIndex == getColumnCount()) {
+            beforeIndex -= 1;
+        }
+        super.insertColumn(beforeIndex, col, header, footer);
+    }
+
+    protected int getTableBodyHeight() {
+        TableSectionElement body = getTableBodyElement();
+        int h = getTableBodyElement().getOffsetHeight();
+        return getTableBodyElement().getOffsetHeight();
+    }
+
+    protected void resizeColumn(Column<T, ?> target, int colWidth) {
+        int tableWidth = tableHeader.getOffsetWidth();
+        int columnCount = getColumnCount();
+        int colIndex = -1;
+        int lastColIndex = columnCount - 1;
+        int residualWidth = 0;
+        for (int i = 0; i < lastColIndex; i++) {
+            Column<T, ?> column = getColumn(i);
+            if (column != target) {
+                residualWidth += getHeaderOffsetWidth(i);
+            } else {
+                colIndex = i;
+            }
+        }
+        if (colIndex == lastColIndex) {
+            return;
+        }
+        int currColWidth = getHeaderOffsetWidth(colIndex);
+        int lastColWidth = getHeaderOffsetWidth(lastColIndex);
+        int borderWidth = tableWidth - residualWidth - currColWidth - lastColWidth;
+
+        int newTableWidth = residualWidth + colWidth + borderWidth;
+        lastColWidth = Math.max(minWidth - newTableWidth, 0);
+
+        setColumnWidth(target, colWidth + "px");
+        setColumnWidth(getColumn(columnCount - 1), lastColWidth + "px");
+        setTableWidth(newTableWidth + lastColWidth, com.google.gwt.dom.client.Style.Unit.PX);
+    }
+
+    protected int getHeaderOffsetWidth(int index) {
+        return tableHeader.ensureTableColElement(index).getOffsetWidth();
+    }
+
+    static class MyResizableHeader<T> extends ResizableHeader<T> {
+        private final MyDataGrid<T> dataGrid;
+
+        public MyResizableHeader(String title, Column<T, ?> column, MyDataGrid<T> dataGrid) {
+            super(title, dataGrid, column);
+            this.dataGrid = dataGrid;
+        }
+
+        @Override
+        protected int getTableBodyHeight() {
+            return dataGrid.getTableBodyHeight();
+        }
+
+        @Override
+        protected void resizeColumn(Column<T, ?> column, int width) {
+            dataGrid.resizeColumn(column, width);
+        }
+    }
+
+}
--- prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/editorapp/client/SdrfSectionView.java
+++ prototypes/other/src/main/java/uk/ac/ebi/fg/annotare2/prototypes/editorapp/client/SdrfSectionView.java
@@ -1,20 +1,44 @@
 package uk.ac.ebi.fg.annotare2.prototypes.editorapp.client;
 
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerManager;
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.IsWidget;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.cellview.client.*;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.view.client.*;
+
+import java.util.*;
 
 /**
  * @author Olga Melnichuk
  */
 public class SdrfSectionView extends Composite implements IsWidget {
 
+    private static final int PAGE_SIZE = 50;
+
     @UiField
     Label title;
+
+    @UiField
+    DockLayoutPanel tablePanel;
+
+    private Button columnsButton;
+
+    private final List<SdrfRow> allRows = new ArrayList<SdrfRow>();
+
+    private final List<SdrfColumn> allColumns = new ArrayList<SdrfColumn>();
 
     interface Binder extends UiBinder<Widget, SdrfSectionView> {
     }
@@ -25,5 +49,180 @@ public class SdrfSectionView extends Composite implements IsWidget {
         initWidget(widget);
 
         title.setText(section);
+        initTable(allRows, allColumns);
     }
+
+    private void initTable(List<SdrfRow> rows, List<SdrfColumn> columns) {
+        MyDataGridResources resources = com.google.gwt.core.shared.GWT.create(MyDataGridResources.class);
+        MyDataGrid<SdrfRow> dataGrid = new MyDataGrid<SdrfRow>(PAGE_SIZE, resources);
+        dataGrid.setEmptyTableWidget(new Label("There's no data yet, come later"));
+        dataGrid.setMinimumTableWidthInPx(tablePanel.getOffsetWidth());
+
+        final SelectionModel<SdrfRow> selectionModel =
+                new MultiSelectionModel<SdrfRow>(new ProvidesKey<SdrfRow>() {
+                    @Override
+                    public Object getKey(SdrfRow item) {
+                        return item.getIndex();
+                    }
+                });
+
+        dataGrid.setSelectionModel(selectionModel, DefaultSelectionEventManager.<SdrfRow>createCheckboxManager());
+
+        SimplePager.Resources pagerResources = com.google.gwt.core.shared.GWT.create(SimplePager.Resources.class);
+        SimplePager pager = new SimplePager(SimplePager.TextLocation.CENTER, pagerResources, false, 0, true);
+        pager.setDisplay(dataGrid);
+
+        ListDataProvider<SdrfRow> dataProvider = new ListDataProvider<SdrfRow>();
+        dataProvider.addDataDisplay(dataGrid);
+        dataProvider.getList().addAll(rows);
+
+        ColumnSortEvent.ListHandler<SdrfRow> sortHandler =
+                new ColumnSortEvent.ListHandler<SdrfRow>(dataProvider.getList());
+        dataGrid.addColumnSortHandler(sortHandler);
+
+        initDefaultColumns(dataGrid, sortHandler);
+
+        for (SdrfColumn column : columns) {
+            addColumn(column);
+        }
+
+        columnsButton = new Button();
+        HorizontalPanel tableBar = new HorizontalPanel();
+        tableBar.add(pager);
+        tableBar.add(columnsButton);
+
+        tablePanel.addNorth(tableBar, 40);
+        tablePanel.add(dataGrid);
+    }
+
+    private void initDefaultColumns(MyDataGrid<SdrfRow> dataGrid, ColumnSortEvent.ListHandler<SdrfRow> sortHandler) {
+        addIndexColumn(dataGrid, sortHandler);
+        addCheckBoxColumn(dataGrid);
+    }
+
+    private void addCheckBoxColumn(final MyDataGrid<SdrfRow> dataGrid) {
+        Column<SdrfRow, Boolean> checkboxColumn = new Column<SdrfRow, Boolean>(new CheckboxCell(true, false)) {
+            @Override
+            public Boolean getValue(SdrfRow object) {
+                return dataGrid.getSelectionModel().isSelected(object);
+            }
+        };
+        dataGrid.addColumn(checkboxColumn, new CheckboxHeader());
+        dataGrid.setColumnWidth(dataGrid.getColumnCount2() - 1, 40, Style.Unit.PX);
+    }
+
+    private void addIndexColumn(MyDataGrid<SdrfRow> dataGrid, ColumnSortEvent.ListHandler<SdrfRow> sortHandler) {
+        Column<SdrfRow, String> column = new Column<SdrfRow, String>(new TextCell()) {
+            @Override
+            public String getValue(SdrfRow row) {
+                return row.getIndex() + "";
+            }
+        };
+        sortHandler.setComparator(column, new Comparator<SdrfRow>() {
+            @Override
+            public int compare(SdrfRow o1, SdrfRow o2) {
+                if (o1 == o2) {
+                    return 0;
+                }
+                int v1 = o1.getIndex();
+                int v2 = o2.getIndex();
+                return new Integer(v1).compareTo(v2);
+            }
+        });
+        column.setSortable(true);
+        dataGrid.addColumn("+", column);
+        dataGrid.setColumnWidth(dataGrid.getColumnCount2() - 1, 50, Style.Unit.PX);
+    }
+
+    private void addColumn(SdrfColumn column) {
+        //TODO
+    }
+
+    private class CheckboxHeader extends Header<Boolean> implements HasValue<Boolean> {
+
+        private boolean checked;
+        private HandlerManager handlerManager;
+
+        public CheckboxHeader() {
+            super(new CheckboxCell());
+            checked = false;
+        }
+
+        @Override
+        public Boolean getValue() {
+            return checked;
+        }
+
+        @Override
+        public void onBrowserEvent(Cell.Context context, Element elem, NativeEvent nativeEvent) {
+            int eventType = Event.as(nativeEvent).getTypeInt();
+            if (eventType == Event.ONCHANGE) {
+                nativeEvent.preventDefault();
+                //use value setter to easily fire change event to handlers
+                setValue(!checked, true);
+            }
+        }
+
+        @Override
+        public HandlerRegistration addValueChangeHandler(ValueChangeHandler<Boolean> handler) {
+            return ensureHandlerManager().addHandler(ValueChangeEvent.getType(), handler);
+        }
+
+        @Override
+        public void fireEvent(GwtEvent<?> event) {
+            ensureHandlerManager().fireEvent(event);
+        }
+
+        @Override
+        public void setValue(Boolean value) {
+            checked = value;
+        }
+
+        @Override
+        public void setValue(Boolean value, boolean fireEvents) {
+            checked = value;
+            if (fireEvents) {
+                ValueChangeEvent.fire(this, value);
+            }
+        }
+
+        private HandlerManager ensureHandlerManager() {
+            if (handlerManager == null) {
+                handlerManager = new HandlerManager(this);
+            }
+            return handlerManager;
+        }
+    }
+
+    private static class SdrfRow {
+        private final Map<SdrfColumn, String> values = new HashMap<SdrfColumn, String>();
+        private final int index;
+
+        private SdrfRow(int index) {
+            this.index = index;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+    }
+
+    public static enum SdrfColumn {
+        CHARCTERISTIC("Characteristic"),
+        FACTOR_VALUE("Factor Value"),
+        ARRAY_DESIGN("Array Design"),
+        LABEL("Label"),
+        MATERIAL_TYPE("Material Type"),
+        PROVIDER("Provider"),
+        TECHNOLOGY_TYPE("Technology Type"),
+        COMMENT("Comment"),
+        PROTOCOL("Protocol");
+
+        private final String title;
+
+        private SdrfColumn(String title) {
+            this.title = title;
+        }
+    }
+
 }
