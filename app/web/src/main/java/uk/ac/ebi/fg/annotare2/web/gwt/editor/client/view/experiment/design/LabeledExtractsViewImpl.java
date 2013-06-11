8eee80ce083e1568d28ae5021fbc36aeae54ca0c
olgamelnichuk <olkin@ebi.ac.uk>
2013-06-11 17:04:59 +0100

    started to work on label assignment UI

---
+++ app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/editor/client/view/experiment/design/LabeledExtractsViewImpl.java
@@ -0,0 +1,9 @@
+package uk.ac.ebi.fg.annotare2.web.gwt.editor.client.view.experiment.design;
+
+import com.google.gwt.user.client.ui.Composite;
+
+/**
+ * @author Olga Melnichuk
+ */
+public class LabeledExtractsViewImpl extends Composite implements LabeledExtractsView {
+}
