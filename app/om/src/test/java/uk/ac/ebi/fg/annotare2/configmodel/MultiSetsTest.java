8994e2e4aecbf6e5383e73e11501f85c16aa12c9
olgamelnichuk <olkin@ebi.ac.uk>
2013-08-23 12:09:57 +0100

    using org.junit.Assert.*;

--- app/om/src/test/java/uk/ac/ebi/fg/annotare2/configmodel/MultiSetsTest.java
+++ app/om/src/test/java/uk/ac/ebi/fg/annotare2/configmodel/MultiSetsTest.java
@@ -21,9 +21,7 @@ import org.junit.Test;
 import java.util.Set;
 
 import static java.util.Arrays.asList;
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author Olga Melnichuk
