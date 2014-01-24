7bf7dd460d3ad68ecd1ad78ea58539603c333ae6
arrayexpress <arrayexpress@ebi.ac.uk>
2014-01-24 12:34:33 +0000

    Merged all DB schema and bootstrap data changes prior to first public beta

--- app/om/src/main/resources/uk/ac/ebi/fg/annotare2/db/changelog/bootstrap-1.1.sql
+++ app/om/src/main/resources/uk/ac/ebi/fg/annotare2/db/changelog/bootstrap-1.1.sql
@@ -1,9 +1,10 @@
 /* INSERT DEFAULT USER */
 
-INSERT INTO users (email, password) VALUES ('user@ebi.ac.uk', 'ee11cbb19052e40b07aac0ca060c23ee');
+INSERT INTO users (name, email, password, emailVerified) VALUES ('Annotare curator', 'curator@ebi.ac.uk', 'b06bb1bec903b5f87a7a482cd8385267', 1);
 SET @userId = LAST_INSERT_ID();
 
 INSERT INTO user_roles (user, role) VALUES (@userId, 'AUTHENTICATED');
+INSERT INTO user_roles (user, role) VALUES (@userId, 'CURATOR');
 
 /* INSERT SUBMISSION ACL */
 
@@ -11,6 +12,7 @@ INSERT INTO acl (aclType) VALUES ('SUBMISSION');
 SET @aclId = LAST_INSERT_ID();
 
 INSERT INTO acl_entries (acl, role, permission) VALUES (@aclId, 'AUTHENTICATED', 'CREATE');
+INSERT INTO acl_entries (acl, role, permission) VALUES (@aclId, 'CREATOR', 'VIEW');
 INSERT INTO acl_entries (acl, role, permission) VALUES (@aclId, 'OWNER', 'VIEW');
 INSERT INTO acl_entries (acl, role, permission) VALUES (@aclId, 'OWNER', 'UPDATE');
 INSERT INTO acl_entries (acl, role, permission) VALUES (@aclId, 'CURATOR', 'VIEW');
---
+++ app/om/src/main/resources/uk/ac/ebi/fg/annotare2/db/changelog/schema-1.1.sql
@@ -0,0 +1,100 @@
+CREATE TABLE users (
+  id                      BIGINT AUTO_INCREMENT NOT NULL,
+  name                    VARCHAR(255)          NOT NULL,
+  email                   VARCHAR(80),
+  emailVerified           TINYINT(1)            NOT NULL
+                            DEFAULT 0,
+  password                VARCHAR(255),
+  passwordChangeRequested TINYINT(1)            NOT NULL
+                            DEFAULT 0,
+  verificationToken       VARCHAR(255),
+  PRIMARY KEY (id),
+  CONSTRAINT unique_email UNIQUE (email)
+)
+  ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+CREATE TABLE user_roles (
+  id   BIGINT AUTO_INCREMENT NOT NULL,
+  user BIGINT                NOT NULL,
+  role ENUM('CREATOR',
+            'OWNER',
+            'AUTHENTICATED',
+            'CURATOR',
+            'ADMIN')         NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT FOREIGN KEY (user) REFERENCES users (id)
+    ON DELETE CASCADE,
+  CONSTRAINT unique_user_role UNIQUE (user, role)
+)
+  ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+CREATE TABLE acl (
+  id      BIGINT AUTO_INCREMENT NOT NULL,
+  aclType ENUM('SUBMISSION')    NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT unique_acl_type UNIQUE (aclType)
+)
+  ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+CREATE TABLE acl_entries (
+  id         BIGINT AUTO_INCREMENT             NOT NULL,
+  acl        BIGINT                            NOT NULL,
+  role       ENUM('CREATOR',
+                  'OWNER',
+                  'AUTHENTICATED',
+                  'CURATOR',
+                  'ADMIN')                     NOT NULL,
+  permission ENUM ('CREATE', 'UPDATE', 'VIEW') NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT FOREIGN KEY (acl) REFERENCES acl (id)
+    ON DELETE CASCADE,
+  CONSTRAINT unique_acl_role_permission UNIQUE (acl, role, permission)
+)
+  ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+CREATE TABLE submissions (
+  id                BIGINT AUTO_INCREMENT              NOT NULL,
+  created           TIMESTAMP                          NOT NULL
+                      DEFAULT '0000-00-00 00:00:00',
+  updated           TIMESTAMP                          NOT NULL
+                      DEFAULT CURRENT_TIMESTAMP
+                      ON UPDATE CURRENT_TIMESTAMP,
+  status            ENUM('IN_PROGRESS',
+                         'SUBMITTED',
+                         'IN_CURATION',
+                         'PRIVATE_IN_AE',
+                         'PUBLIC_IN_AE')               NOT NULL,
+  type              ENUM('EXPERIMENT', 'ARRAY_DESIGN') NOT NULL,
+  accession         VARCHAR(50),
+  title             VARCHAR(500),
+  acl               BIGINT,
+  createdBy         BIGINT                             NOT NULL,
+  ownedBy           BIGINT                             NOT NULL,
+  experiment        MEDIUMTEXT,
+  arrayDesignHeader TEXT,
+  arrayDesignBody   MEDIUMTEXT,
+  subsTrackingId    INT,
+  deleted           TINYINT(1)                         NOT NULL
+                      DEFAULT 0,
+  PRIMARY KEY (id),
+  CONSTRAINT FOREIGN KEY (acl) REFERENCES acl (id),
+  CONSTRAINT FOREIGN KEY (createdBy) REFERENCES users (id)
+    ON DELETE CASCADE,
+  CONSTRAINT FOREIGN KEY (ownedBy) REFERENCES users (id)
+)
+  ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+CREATE TABLE data_files (
+  id       BIGINT AUTO_INCREMENT                   NOT NULL,
+  created  TIMESTAMP                               NOT NULL,
+  status   ENUM('TO_BE_STORED', 'STORED', 'ERROR') NOT NULL,
+  fileName VARCHAR(255)                            NOT NULL,
+  digest   VARCHAR(255),
+  ownedBy  BIGINT                                  NOT NULL,
+  deleted  TINYINT(1)                              NOT NULL
+             DEFAULT 0,
+  PRIMARY KEY (id),
+  CONSTRAINT FOREIGN KEY (ownedBy) REFERENCES submissions (id)
+    ON DELETE CASCADE
+)
+  ENGINE = InnoDB DEFAULT CHARSET = UTF8;
