f95f88eca0490766565fb3d57713063dc4f9bb6e
arrayexpress <arrayexpress@ebi.ac.uk>
2014-05-08 22:28:33 +0100

    Added multi-upload capability (except old IEs); fixed inability to upload files with Unicode characters

--- app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/editor/client/view/widget/DataFileHttpUploadView.java
+++ app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/editor/client/view/widget/DataFileHttpUploadView.java
@@ -17,19 +17,18 @@
 package uk.ac.ebi.fg.annotare2.web.gwt.editor.client.view.widget;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.Constants;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
-import gwtupload.client.IFileInput;
-import gwtupload.client.IUploadStatus;
-import gwtupload.client.IUploader;
-import gwtupload.client.MultiUploader;
+import gwtupload.client.*;
+import uk.ac.ebi.fg.annotare2.web.gwt.common.shared.exepriment.HttpFileInfo;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
-import java.util.Set;
 
 import static gwtupload.client.IUploadStatus.Status.SUCCESS;
 
@@ -47,35 +46,53 @@ public class DataFileHttpUploadView extends Composite {
         Binder BINDER = GWT.create(Binder.class);
     }
 
-    public DataFileHttpUploadView() {
-        initWidget(Binder.BINDER.createAndBindUi(this));
-
-        final MultiUploader uploader = new MultiUploader(IFileInput.FileInputType.LABEL);
-        uploader.avoidRepeatFiles(false);
-        Set<IUploadStatus.CancelBehavior> cancelBehaviors = new HashSet<IUploadStatus.CancelBehavior>();
-        cancelBehaviors.add(IUploadStatus.CancelBehavior.REMOVE_CANCELLED_FROM_LIST);
-        cancelBehaviors.add(IUploadStatus.CancelBehavior.STOP_CURRENT);
-        cancelBehaviors.add(IUploadStatus.CancelBehavior.REMOVE_REMOTE);
-
-        UploadStatus status = new UploadStatus();
-        status.setCancelConfiguration(cancelBehaviors);
-        uploader.setStatusWidget(status);
-        uploader.setAutoSubmit(true);
-
-        uploader.addOnFinishUploadHandler(new IUploader.OnFinishUploaderHandler() {
-            @Override
-            public void onFinish(IUploader iuploader) {
-                if (iuploader.getStatus() == SUCCESS) {
-                    //iuploader.asWidget().removeFromParent();
-                    if (presenter != null) {
-                        List<IUploader.UploadedInfo> uploaded = iuploader.getServerMessage().getUploadedInfos();
-                        for (IUploader.UploadedInfo info : uploaded) {
-                            presenter.fileUploaded(info.getField());
+    class OnFinishUploaderHandler implements IUploader.OnFinishUploaderHandler {
+        @Override
+        public void onFinish(IUploader uploader) {
+            if (uploader.getStatus() == SUCCESS) {
+                uploader.asWidget().removeFromParent();
+                if (presenter != null) {
+                    IFileInput input = uploader.getFileInput();
+                    List<String> fileNames = input.getFilenames();
+                    List<IUploader.UploadedInfo> uploaded = uploader.getServerMessage().getUploadedInfos();
+                    if (uploaded.size() == fileNames.size()) {
+                        List<HttpFileInfo> filesInfo = new ArrayList<HttpFileInfo>(uploaded.size());
+                        for (int i = 0; i < uploaded.size(); ++i) {
+                            filesInfo.add(new HttpFileInfo(uploaded.get(i).getField(), fileNames.get(i)));
                         }
+                        presenter.filesUploaded(filesInfo);
                     }
                 }
             }
-        });
+        }
+    }
+
+    interface UploaderConstants extends IUploader.UploaderConstants {
+
+        @Constants.DefaultStringValue("Select files...")
+        String uploaderBrowse();
+    }
+
+    public static final UploaderConstants I18N_CONSTANTS = GWT.create(UploaderConstants.class);
+
+    public DataFileHttpUploadView() {
+        initWidget(Binder.BINDER.createAndBindUi(this));
+
+        //UploadStatus status = new UploadStatus();
+        //status.setCancelConfiguration(cancelBehaviors);
+
+        IUploadStatus status = new BaseUploadStatus();
+        status.setCancelConfiguration(
+                EnumSet.of(
+                        IUploadStatus.CancelBehavior.STOP_CURRENT
+                        , IUploadStatus.CancelBehavior.REMOVE_INVALID
+                        , IUploadStatus.CancelBehavior.REMOVE_CANCELLED_FROM_LIST
+                )
+        );
+        final MultiUploader uploader = new MultiUploader(IFileInput.FileInputType.BUTTON);
+        uploader.setI18Constants(I18N_CONSTANTS);
+        uploader.avoidRepeatFiles(true);
+        uploader.addOnFinishUploadHandler(new OnFinishUploaderHandler());
         panel.add(uploader);
     }
 
@@ -84,6 +101,6 @@ public class DataFileHttpUploadView extends Composite {
     }
 
     public interface Presenter {
-        void fileUploaded(String name);
+        void filesUploaded(List<HttpFileInfo> filesInfo);
     }
 }
