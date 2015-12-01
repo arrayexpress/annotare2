6c196a1ea698cd5879b52b986b69d21445cff343
arrayexpress <arrayexpress@ebi.ac.uk>
2015-12-01 11:12:14 +0000

    Updated library wrapper

--- prototypes/resumable/src/main/java/uk/ac/ebi/fg/annotare/prototype/upload/resumable/client/ResumableUpload.java
+++ prototypes/resumable/src/main/java/uk/ac/ebi/fg/annotare/prototype/upload/resumable/client/ResumableUpload.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.fg.annotare.prototype.upload.resumable.client;
 
+import com.google.gwt.core.client.Callback;
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Element;
 
@@ -8,7 +9,7 @@ public class ResumableUpload extends JavaScriptObject {
     protected ResumableUpload() {
     }
 
-    public static final native void init(String url)/*-{
+    public static final native void init(String url) /*-{
         this.r = new $wnd.Resumable({
             target: url,
             query: {xxx: 'xxx'},
@@ -20,7 +21,25 @@ public class ResumableUpload extends JavaScriptObject {
         if (undefined !== this.r) {
             this.r.assignBrowse(element);
         } else {
-            console.error('Please init the library first');
+            console.error('resumable.assignBrowse: please init the library first');
+        }
+    }-*/;
+
+    public static final native void upload() /*-{
+        if (undefined !== this.r) {
+            this.r.upload();
+        } else {
+            console.error('resumable.upload: please init the library first');
+        }
+    }-*/;
+
+    public static final native void on(String eventType, Callback callback) /*-{
+        if (undefined !== this.r) {
+            this.r.on(eventType, function() {
+                callback;
+            });
+        } else {
+            console.error('resumable.on: please init the library first');
         }
     }-*/;
 }
