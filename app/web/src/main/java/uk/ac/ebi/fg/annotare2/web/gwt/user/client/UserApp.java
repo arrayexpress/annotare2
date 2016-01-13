c136504172e25582eb0a6e71b9e3b0f11e239712
arrayexpress <arrayexpress@ebi.ac.uk>
2016-01-13 15:55:58 +0000

    Removed staff shortage notice 2015

--- app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/user/client/UserApp.java
+++ app/web/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/user/client/UserApp.java
@@ -88,7 +88,7 @@ public class UserApp implements EntryPoint {
         historyHandler.handleCurrentHistory();
 
         ServerWatchdog.start();
-        showNotice();
+        //showNotice();
     }
 
     private final static String NOTICE_COOKIE = "DowntimeNoticeDec2015Shown";
