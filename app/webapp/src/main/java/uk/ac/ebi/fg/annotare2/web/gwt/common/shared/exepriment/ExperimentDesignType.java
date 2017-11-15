9262b0197c53cafa971a8677b4646b66e4b12a8a
Haider Iqbal <haideri@ebi.ac.uk>
2017-11-15 15:51:06 +0000

    Change height of Experiment Setup screen Make experiment design list sort in alphabetically

--- app/webapp/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/common/shared/exepriment/ExperimentDesignType.java
+++ app/webapp/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/common/shared/exepriment/ExperimentDesignType.java
@@ -12,6 +12,7 @@ public enum ExperimentDesignType {
     BINDING_SITE_IDENTIFICATION_DESIGN("binding site identification design","EFO:0004664",SampleAttributeTemplate.IMMUNOPRECIPITATE_ORIGIN),
     CASE_CONTROL_DESIGN("case control design","EFO:0001427",SampleAttributeTemplate.DESEASE_ATTRIBUTE),
     CELL_TYPE_COMPARISON_DESIGN("cell type comparison design","EFO:0001745", SampleAttributeTemplate.CELL_TYPE_ORIGIN),
+    CLINICAL_HISTORY_DESIGN("clinical history design","EFO:0000352",SampleAttributeTemplate.CLINICAL_HISTORY_ATTRIBUTE),
     COMPOUND_TREATMENT_DESIGN("compound treatment design","EFO:0001755",SampleAttributeTemplate.COMPOUND_ATTRIBUTE,SampleAttributeTemplate.DOSE_ORIGIN),
     DEVELOPMENT_DIFFERENTIAL_DESIGN("development or differentiation design","EFO:0001746",SampleAttributeTemplate.DEVELOPMENTAL_STAGE_ORIGIN),
     DISEASE_STATE_DESIGN("disease state design","EFO:0001756",SampleAttributeTemplate.DESEASE_ATTRIBUTE),
@@ -19,13 +20,12 @@ public enum ExperimentDesignType {
     GENETIC_MODIFICATION_DESIGN("genetic modification design","EFO:0001758",SampleAttributeTemplate.GENOTYPE_ATTRIBUTE),
     GENOTYPE_DESIGN("genotype design","EFO:0001748",SampleAttributeTemplate.GENOTYPE_ATTRIBUTE),
     GROWTH_CONDITION_DESIGN("growth condition design","EFO:0001759",SampleAttributeTemplate.GROWTH_CONDITION_ORIGIN),
+    INJURY_DESIGN("injury design","EFO:0001760",SampleAttributeTemplate.INJURY_ATTRIBUTE),
     ORGANISM_PART_COMPARISON_DESIGN("organism part comparison design","EFO:0001750",SampleAttributeTemplate.ORGANISM_PART_ATTRIBUTE),
     SEX_DESIGN("sex design","EFO:0001752",SampleAttributeTemplate.SEX_ORIGIN),
     SPECIES_DESIGN("species design","EFO:0001753",SampleAttributeTemplate.ORGANISM_ATTRIBUTE),
     STIMULUS_STRESS_DESIGN("stimulus or stress design","EFO:0001762",SampleAttributeTemplate.STIMULUS_ATTRIBUTE),
     STAIN_LINE_DESIGN("strain or line design","EFO:0001754",SampleAttributeTemplate.STRAIN_ATTRIBUTE),
-    INJURY_DESIGN("injury design","EFO:0001760",SampleAttributeTemplate.INJURY_ATTRIBUTE),
-    CLINICAL_HISTORY_DESIGN("clinical history design","EFO:0000352",SampleAttributeTemplate.CLINICAL_HISTORY_ATTRIBUTE),
     TIME_SERIES_DESIGN("time series design","EFO:0001779",SampleAttributeTemplate.TIME_ATTRIBUTE);
 
     private final String label;
