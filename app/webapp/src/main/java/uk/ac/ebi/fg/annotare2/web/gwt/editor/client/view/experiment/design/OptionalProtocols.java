3a985491957a4e6050543257dafebc7aa832eebc
Haider Iqbal <haideri@ebi.ac.uk>
2019-02-20 12:07:21 +0000

    - Fix Growth protocol not showing up for new Plant templates

--- app/webapp/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/editor/client/view/experiment/design/OptionalProtocols.java
+++ app/webapp/src/main/java/uk/ac/ebi/fg/annotare2/web/gwt/editor/client/view/experiment/design/OptionalProtocols.java
@@ -8,13 +8,15 @@ import java.util.EnumSet;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static uk.ac.ebi.fg.annotare2.submission.model.ExperimentProfileType.*;
 
 /**
  * Created by haideri on 17/05/2017.
  */
 public enum OptionalProtocols {
 
-    GROWTH_PROTOCOL("growth protocol",ExperimentProfileType.PLANT_ONE_COLOR_MICROARRAY,ExperimentProfileType.PLANT_TWO_COLOR_MICROARRAY,ExperimentProfileType.PLANT_SEQUENCING),
+    GROWTH_PROTOCOL("growth protocol", PLANT_ONE_COLOR_MICROARRAY, PLANT_TWO_COLOR_MICROARRAY, PLANT_SEQUENCING,
+                                        PLANT_METHYLATION_MICROARRAY, SINGLE_CELL_PLANT_SEQUENCING),
     TREATMENT_PROTOCOL("treatment protocol"),
     NORMALIZATION_DATA_TRANSFORMATION_PROTOCOL("normalization data transformation protocol"),
     CONVERSION_PROTOCOL("conversion protocol"),
