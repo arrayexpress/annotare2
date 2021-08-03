23c4960fc4d6536954c4f05b9a098f42477af483
sandeepkurri <sandeepkurri@ebi.ac.uk>
2021-08-03 11:49:35 +0100

    Rsync options modified to check md5 before copy.

--- app/integration/ae-integration/src/main/java/uk/ac/ebi/fg/annotare2/integration/AeIntegrationWatchdog.java
+++ app/integration/ae-integration/src/main/java/uk/ac/ebi/fg/annotare2/integration/AeIntegrationWatchdog.java
@@ -57,6 +57,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Collection;
@@ -402,7 +404,7 @@ public class AeIntegrationWatchdog {
                     logger.debug("Processing data file {}", dataFile.getName());
                     if (DataFileStatus.STORED == dataFile.getStatus()) {
                         URI destinationURI = (isSequencing && rawDataFiles.contains(dataFile))
-                                ? new URI(ftpManager.getDirectory(ftpSubDirectory) + dataFile.getName())
+                                ? new URI(ftpManager.getDirectory(ftpSubDirectory) + URLEncoder.encode(dataFile.getName(), StandardCharsets.UTF_8.toString()))
                                 : new File(unpackedExportDirectory, dataFile.getName()).toURI();
                         DataFileHandle source = dataFileManager.getFileHandle(dataFile);
                         DataFileHandle destination = source.copyTo(destinationURI);
